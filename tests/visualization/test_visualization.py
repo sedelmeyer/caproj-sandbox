4a45b11a31feac137ea2f55f1e03ca0da68a984d
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-06-02 06:11:55 -0400

    test(viz): add simple setup teardown

--- tests/visualization/test_visualization.py
+++ tests/visualization/test_visualization.py
@@ -1,5 +1,24 @@
 from unittest import TestCase, mock
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
 from caproj import visualization as vis
 
 
+class TestSavePlot(TestCase):
+    """Test save_plot function"""
+
+    def setUp(self):
+        self.data = pd.DataFrame(
+            {
+                'x': [0, 1, 2, 3],
+                'y': [2, 3, 4, 5]
+            }
+        )
+        return super().setUp()
+
+    def tearDown(self):
+        self.data.dispose()
+        return super().tearDown()
