e3b4ca063fabd92acb7ee621504b7cc6470e21d4
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-06-02 06:28:35 -0400

    test(viz): add save_plot tests

--- tests/visualization/test_visualization.py
+++ tests/visualization/test_visualization.py
@@ -1,4 +1,6 @@
+import os
 from unittest import TestCase, mock
+from tempfile import TemporaryDirectory
 
 import numpy as np
 import pandas as pd
@@ -11,6 +13,7 @@ class TestSavePlot(TestCase):
     """Test save_plot function"""
 
     def setUp(self):
+        """Set up data for tests"""
         self.data = pd.DataFrame(
             {
                 'x': [0, 1, 2, 3],
@@ -19,6 +22,17 @@ class TestSavePlot(TestCase):
         )
         return super().setUp()
 
-    def tearDown(self):
-        self.data.dispose()
-        return super().tearDown()
+    def test_save_plot(self):
+        """Test save_plot function"""
+        with TemporaryDirectory() as tmp:
+            plt.plot(self.data['x'], self.data['y'])
+            fp = os.path.join(tmp, 'test.png')
+            vis.save_plot(plt_object=plt, savepath=fp)
+            assert os.path.exists(fp)
+
+    def test_save_plot_none(self):
+        """Test save_plot function passes with no savepath"""
+        with TemporaryDirectory() as tmp:
+            plt.plot(self.data['x'], self.data['y'])
+            vis.save_plot(plt_object=plt, savepath=None)
+            assert not os.path.exists(fp)
