9710ec598bb1f39f01c7b541d7d7f144ee6d4829
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-06-08 06:44:25 -0400

    test(log): move json.load patch to setUp for setup_logging func TestCase

--- tests/logger/test_logger.py
+++ tests/logger/test_logger.py
@@ -8,29 +8,24 @@ from caproj import logger
 class TestLoggingSetup(TestCase):
     """Test setup_logging function"""
 
-    # def setUp(self):
-    #     """Set up data for tests"""
-    #     self.data = pd.DataFrame(
-    #         {
-    #             'x': [0, 1, 2, 3],
-    #             'y': [2, 3, 4, 5]
-    #         }
-    #     )
-    #     return super().setUp()
+    def setUp(self):
+        """Set and clean up mock.patch objects for tests"""
+        patcher = mock.patch('caproj.logger.json.load')
+        self.mock_load = patcher.start()
+        self.addCleanup(patcher.stop)
 
     def test_setup_logging_basicConfig(self):
         """Test setup_logging initializes with basicConfig"""
         with mock.patch(
-            "caproj.logger.logging.basicConfig"
+            'caproj.logger.logging.basicConfig'
         ) as basicConfig_patch:
             logger.setup_logging(default_path='foo.json', env_key='foo')
             assert basicConfig_patch.called
 
-    @mock.patch('caproj.logger.json.load')
-    def test_setup_logging_dictConfig(self, mock_load):
+    def test_setup_logging_dictConfig(self):
         """Test setup_logging initializes with dictConfig from file"""
         with TemporaryDirectory() as tmp:
-            fp = os.path.join(tmp, "foo.json")
+            fp = os.path.join(tmp, 'foo.json')
             open(fp, 'a').close()
             with mock.patch(
                 'caproj.logger.logging.config.dictConfig'
