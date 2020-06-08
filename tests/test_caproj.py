97b0244769aaf31fb64f21a23a861191b9b18911
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-06-08 19:23:29 -0400

    test(log): add default NullHandler tests

--- tests/test_caproj.py
+++ tests/test_caproj.py
@@ -1,4 +1,23 @@
+from unittest import TestCase
+
+import logging
+
 from caproj.cli import main
+
 
 def test_main():
     main([])
+
+
+class TestNullHandler(TestCase):
+    """Test default logger is initialized with module"""
+
+    def test_default_logging_handler(self):
+        """Ensure only one handler is initialized by default"""
+        logger = logging.getLogger('caproj')
+        self.assertEqual(len(logger.handlers), 1)
+
+    def test_logging_nullhandler(self):
+        """Ensure logging.nullHandler is initialized with module"""
+        logger = logging.getLogger('caproj')
+        self.assertIsInstance(logger.handlers[0], logging.NullHandler)
