6d14d09f57184e13f0540aed9e527b45ef0568a9
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-07-06 07:24:46 -0400

    src(logger): fix merge conflict

--- src/caproj/logger/__init__.py
+++ src/caproj/logger/__init__.py
@@ -27,7 +27,8 @@ def start_logging(
     default_level='INFO',
     env_key='LOG_CFG'
 ):
-    """Set up logging configuration for ``{{cookiecutter.package_name}}`` package
+    """Set up logging configuration for ``caproj`` package
+
     :param default_path: string file path for json formatted
                          logging configuration file (default is
                          'logging.json')
@@ -73,13 +74,11 @@ def logfunc(orig_func=None, log=None,
             funcname=False, argvals=False,
             docdescr=False, runtime=False):
     """Wrap function call to provide log information when function is called
+
     This function acts as a ``functools.wraps`` decorator for decorating
     functions or methods to provide logging functionality to log details
     of the decorated function
-<<<<<<< HEAD
 
-=======
->>>>>>> develop
     :param orig_func: NoneType placeholder parameter
     :param log: logging.getLogger object for logging, default is None
     :param funcname: boolean indicating whether to log name of function,
@@ -91,19 +90,11 @@ def logfunc(orig_func=None, log=None,
     :param runtime: boolean indicating whether to log function execution
                     runtime in seconds, default is False
     :return: ``functools.wraps`` wrapper function
-<<<<<<< HEAD
 
     .. note:: All logs are generate at the 'INFO' logging level
 
     Example::
-
         log = logging.getLogger(__name__)
-
-=======
-    .. note:: All logs are generate at the 'INFO' logging level
-    Example::
-        log = logging.getLogger(__name__)
->>>>>>> develop
         @logfunc(log=log, funcname=True, runtime=True)
         def some_function(arg1, **kwargs):
             ...
