0b0fe94c7e4c31ba27e75a0dde86af74f624fe6b
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-07-13 19:27:58 -0400

    test(data): add setUp for BaseData.set_dtypes test case

--- tests/data/test_base.py
+++ tests/data/test_base.py
@@ -18,23 +18,17 @@ class BaseDataIOTests(TestCase):
     def setUp(self):
         """Set up data for tests"""
         self.x = [0, 1, 1, 0]
-        self.data = pd.DataFrame(
-            {
-                'PID': self.x,
-                'y': [2, 3, 4, 5]
-            }
-        )
+        self.data = pd.DataFrame({"PID": self.x, "y": [2, 3, 4, 5]})
         return super().setUp()
 
     def test_from_file_csv(self):
         """Ensure csv is read and stored to BaseDataClass class"""
         with tempfile.TemporaryDirectory() as tmp:
-            fp = os.path.join(tmp, 'test.csv')
+            fp = os.path.join(tmp, "test.csv")
             self.data.to_csv(fp, index=False)
             df_read = BaseData.from_file(fp).df
             self.assertEqual(
-                pd.testing.assert_frame_equal(self.data, df_read),
-                None,
+                pd.testing.assert_frame_equal(self.data, df_read), None,
             )
 
     def test_from_file_fail(self):
@@ -46,18 +40,17 @@ class BaseDataIOTests(TestCase):
     def test_from_file_inputdf_persists(self):
         """Ensure df_input persist only when specified"""
         with tempfile.TemporaryDirectory() as tmp:
-            fp = os.path.join(tmp, 'test.csv')
+            fp = os.path.join(tmp, "test.csv")
             self.data.to_csv(fp, index=False)
             df_read = BaseData.from_file(fp, copy_input=True).df_input
             self.assertEqual(
-                pd.testing.assert_frame_equal(self.data, df_read),
-                None,
+                pd.testing.assert_frame_equal(self.data, df_read), None,
             )
 
     def test_from_file_inputdf_not_created(self):
         """Ensure df_input persist only when specified"""
         with tempfile.TemporaryDirectory() as tmp:
-            fp = os.path.join(tmp, 'test.csv')
+            fp = os.path.join(tmp, "test.csv")
             self.data.to_csv(fp, index=False)
             with self.assertRaises(AttributeError):
                 BaseData.from_file(fp).df_input
@@ -66,8 +59,7 @@ class BaseDataIOTests(TestCase):
         """Ensure dataframe object is read and stored to BaseDataClass class"""
         df_read = BaseData.from_object(self.data).df
         self.assertEqual(
-            pd.testing.assert_frame_equal(self.data, df_read),
-            None,
+            pd.testing.assert_frame_equal(self.data, df_read), None,
         )
 
     def test_from_object_class(self):
@@ -75,14 +67,15 @@ class BaseDataIOTests(TestCase):
         Base_object = BaseData.from_object(self.data)
         df_read = BaseData.from_object(Base_object).df
         self.assertEqual(
-            pd.testing.assert_frame_equal(self.data, df_read),
-            None,
+            pd.testing.assert_frame_equal(self.data, df_read), None,
         )
 
     def test_from_object_fail(self):
         """Ensure from_object fails elegantly with invalid object"""
+
         class InvalidClass(object):
             pass
+
         Invalid_object = InvalidClass()
         with self.assertRaises(Exception):
             BaseData.from_object(Invalid_object)
@@ -98,8 +91,8 @@ class BaseDataIOTests(TestCase):
     def test_log_record_count(self):
         """Ensure log_record_count returns log"""
         Base_object = BaseData.from_object(self.data)
-        with self.assertLogs('BaseData', level='INFO') as logmsg:
-            Base_object.log_record_count(id_col='PID')
+        with self.assertLogs("BaseData", level="INFO") as logmsg:
+            Base_object.log_record_count(id_col="PID")
             self.assertTrue(len(logmsg) > 0)
 
     def test_log_record_count_init(self):
@@ -117,18 +110,16 @@ class BaseDataReadJsonTests(TestCase):
     def setUp(self):
         """Set up data for tests"""
         # define dict for saving to json
-        self.json_dict = {'a': 1, 'b': 2}
+        self.json_dict = {"a": 1, "b": 2}
         # initialize BaseData class for use in tests
-        self.Base = BaseData(pd.DataFrame(columns=['PID']), copy_input=False)
+        self.Base = BaseData(pd.DataFrame(columns=["PID"]), copy_input=False)
         # use ExitStack for opening a temp directory for use in tests
         with contextlib.ExitStack() as stack:
             # open temp directory context manager
-            self.tmpdir = stack.enter_context(
-                tempfile.TemporaryDirectory()
-            )
-            self.filepath = os.path.join(self.tmpdir, 'foo.json')
+            self.tmpdir = stack.enter_context(tempfile.TemporaryDirectory())
+            self.filepath = os.path.join(self.tmpdir, "foo.json")
             # save json to temp dir
-            with open(self.filepath, 'w') as fp:
+            with open(self.filepath, "w") as fp:
                 json.dump(self.json_dict, fp)
             self.assertTrue(os.path.exists(self.filepath))
             # ensure context manager closes after tests
@@ -141,8 +132,8 @@ class BaseDataReadJsonTests(TestCase):
 
     def test_read_json_no_file_exists_log(self):
         """Ensure _read_json returns warning log msg when path doesn't exist"""
-        with self.assertLogs('BaseData', level='WARNING') as logmsg:
-            read_dict = self.Base._read_json('nonexistent path')
+        with self.assertLogs("BaseData", level="WARNING") as logmsg:
+            read_dict = self.Base._read_json("nonexistent path")
             self.assertEqual(read_dict, None)
             self.assertTrue("No data loaded" in logmsg.output[0])
 
@@ -152,8 +143,8 @@ class BaseDataColLintTests(TestCase):
 
     def setUp(self):
         """Set up data for tests"""
-        self.orig_colnames = ['test1', 'test_2', 'test 3', 'test-4']
-        self.linted_colnames = ['test1', 'test_2', 'test_3', 'test_4']
+        self.orig_colnames = ["test1", "test_2", "test 3", "test-4"]
+        self.linted_colnames = ["test1", "test_2", "test_3", "test_4"]
         self.Base = BaseData(
             pd.DataFrame(columns=self.orig_colnames), copy_input=False
         )
@@ -164,19 +155,17 @@ class BaseDataColLintTests(TestCase):
     def test_lint_colnames(self):
         """Ensure lint_colnames fixes column name strings"""
         self.Base.lint_colnames()
-        self.assertListEqual(
-            list(self.Base.df.columns), self.linted_colnames
-        )
+        self.assertListEqual(list(self.Base.df.columns), self.linted_colnames)
 
     def test_lint_colnames_log_changed(self):
         """Ensure lint_colnames method logging works"""
-        with self.assertLogs('BaseData', level='INFO') as logmsg:
+        with self.assertLogs("BaseData", level="INFO") as logmsg:
             self.Base.lint_colnames()
             self.assertTrue("Column names changed" in logmsg.output[0])
 
     def test_lint_colnames_log_not_changed(self):
         """Ensure lint_colnames method logging works"""
-        with self.assertLogs('BaseData', level='INFO') as logmsg:
+        with self.assertLogs("BaseData", level="INFO") as logmsg:
             self.Base_nolint.lint_colnames()
             self.assertTrue("No column names changed" in logmsg.output[0])
 
@@ -186,11 +175,9 @@ class BaseDataColNameTests(TestCase):
 
     def setUp(self):
         """Set up data for tests"""
-        self.orig_colnames = ['a', 'b', 'c', 'PID']
-        self.new_colnames = ['1', '2', '3', 'PID']
-        self.map_dict = dict(
-            zip(self.orig_colnames[:3], self.new_colnames[:3])
-        )
+        self.orig_colnames = ["a", "b", "c", "PID"]
+        self.new_colnames = ["1", "2", "3", "PID"]
+        self.map_dict = dict(zip(self.orig_colnames[:3], self.new_colnames[:3]))
         self.Base = BaseData(
             pd.DataFrame(columns=self.orig_colnames), copy_input=False
         )
@@ -203,35 +190,48 @@ class BaseDataColNameTests(TestCase):
     def test_rename_columns_json(self):
         """Ensure rename_columns renames columns using json dict"""
         with tempfile.TemporaryDirectory() as tmpdir:
-            filepath = os.path.join(tmpdir, 'foo.json')
-            with open(filepath, 'w') as fp:
+            filepath = os.path.join(tmpdir, "foo.json")
+            with open(filepath, "w") as fp:
                 json.dump(self.map_dict, fp)
-            with self.assertLogs('BaseData', level='INFO') as logmsg:
+            with self.assertLogs("BaseData", level="INFO") as logmsg:
                 self.Base.rename_columns(json_path=filepath)
                 self.assertListEqual(
                     list(self.Base.df.columns), self.new_colnames
                 )
-                self.assertTrue('Column names mapped' in logmsg.output[0])
+                self.assertTrue("Column names mapped" in logmsg.output[0])
 
     def test_rename_columns_json_fails_no_file_exists(self):
         """Ensure rename_columns fails elegantly when json does not exist"""
-        with self.assertLogs('BaseData', level='INFO') as logmsg:
-            self.Base.rename_columns(json_path='nonexistent path')
-            self.assertListEqual(
-                list(self.Base.df.columns), self.orig_colnames
-            )
-            self.assertTrue('JSON failed' in ''.join(logmsg.output))
+        with self.assertLogs("BaseData", level="INFO") as logmsg:
+            self.Base.rename_columns(json_path="nonexistent path")
+            self.assertListEqual(list(self.Base.df.columns), self.orig_colnames)
+            self.assertTrue("JSON failed" in "".join(logmsg.output))
 
     def test_rename_columns_log(self):
         """Ensure rename_columns logging works"""
-        with self.assertLogs('BaseData', level='INFO') as logmsg:
+        with self.assertLogs("BaseData", level="INFO") as logmsg:
             self.Base.rename_columns()
-            self.assertListEqual(
-                list(self.Base.df.columns), self.orig_colnames
-            )
+            self.assertListEqual(list(self.Base.df.columns), self.orig_colnames)
             self.assertTrue(
-                'Neither a map_dict nor json_path' in ''.join(logmsg.output)
+                "Neither a map_dict nor json_path" in "".join(logmsg.output)
             )
+
+
+class BaseDataColDtypeTests(TestCase):
+    """Tests to ensure ``BaseData`` column dtype conversion functions properly"""
+
+    def setUp(self):
+        """Set up data for tests"""
+        self.colvalues_dict = {
+            "a": ["1", "2", "3"],
+            "b": [1, 2, "test"],
+            "c": [1, 2, "2020-01-01"],
+            "PID": ["test", "test", "test"],
+        }
+        self.map_dict = dict(zip(self.orig_colnames[:3], self.new_colnames[:3]))
+        self.Base = BaseData(
+            pd.DataFrame().from_dict(self.colvalues_dict), copy_input=False
+        )
 
     def test_set_dtypes_json(self):
         """Ensure set_dtypes converts json input to dict"""
