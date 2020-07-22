231040e1bb1340d2650e9e08724c1c22bfa99d6e
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-07-22 07:39:26 -0400

    test(data): add placeholder tests for make_record_keys

--- tests/data/test_clean.py
+++ tests/data/test_clean.py
@@ -41,3 +41,33 @@ class CleanMixinTests(unittest.TestCase):
         with self.assertLogs("caproj.data.clean", level="INFO") as logmsg:
             self.Base.remove_missing_records(columns="a")
             self.assertTrue(len(logmsg.output) == 3)
+
+
+class MakeIdTests(unittest.TestCase):
+    """Tests to ensure CleanMixin make_record_keys method function properly"""
+
+    def setUp(self):
+        """Set up data for tests"""
+        self.colvalues_dict = {
+            "a": [1, 2, 3],
+            "a": [1, 2, np.nan],
+            "PID": ["test", "test", "test"],
+        }
+        self.Base = CleanMixin()
+        self.Base.df = pd.DataFrame().from_dict(self.colvalues_dict)
+
+    def test_make_record_keys_numbers_concat(self):
+        """Ensure make_record_keys concatenates numeric values instead of adds"""
+        raise NotImplementedError
+
+    def test_make_record_keys_single_col(self):
+        """Ensure make_record_keys works with single column input string"""
+        raise NotImplementedError
+
+    def test_make_record_keys_multi_col(self):
+        """Ensure make_record_keys works with multi-column input list"""
+        raise NotImplementedError
+
+    def test_make_record_keys_log(self):
+        """Ensure make_record_keys generates log"""
+        raise NotImplementedError
