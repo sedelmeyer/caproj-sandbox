542730fbf73af74ca5d6aa3a812c54ae5d83cce6
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-07-24 06:36:45 -0400

    src(data): remove unused imports from caproj.data.clean

--- src/caproj/data/clean.py
+++ src/caproj/data/clean.py
@@ -18,14 +18,10 @@ This module contains BaseData mixin class to clean the NYC Capital Projects data
 
 |
 """
-import json
 import logging
-import math
-import os
-
-import pandas as pd
 
 from caproj.logger import logfunc
+
 
 log = logging.getLogger(__name__)
 """``logging.getLogger`` instance for module"""
